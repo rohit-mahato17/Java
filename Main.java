96afc68744949d5bfc6913a60753de612832c5e4
Rohit Mahato <rohitmahato88935@gmail.com>
2026-08-01 21:01:43 +0530

    java basic

---
+++ Main.java
@@ -0,0 +1,17 @@
+public class Main {
+    public static void main(String args[]){
+
+        //output
+        // System.out.print("Hello world");
+        // System.out.print("Hello world with java\n");
+        // System.out.print("Hello world with java\n");
+        // System.out.print("Hello world with java\n");
+        // System.out.print("Hello in the world \n hello world \n hi");
+
+        //print the pattern
+        System.out.println("*");
+        System.out.println("**");
+        System.out.println("***");
+        System.out.println("****");
+    }
+} 
