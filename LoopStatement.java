3f8d1df66f823b00cc66c20a9c09d6824e0fe397
Rohit Mahato <rohitmahato88935@gmail.com>
2026-08-03 15:42:11 +0530

    Loop Statement

--- LoopStatement.java
+++ LoopStatement.java
@@ -5,11 +5,40 @@ public class LoopStatement {
     public static void main(String[] args) {
         
         Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
+        // int n = sc.nextInt();
         
-        for(int i=1; i<=n; i++){
+        // for(int i=1; i<=n; i++){
+        //     System.out.println(i);
+        // }
+
+        // Sum of input
+
+        // int sum = 0;
+
+        // for(int i=1; i<=n; i++){
+        //    sum += i;
+        // }
+        // System.out.print("The value of sum:");
+        // System.out.println(sum);
+
+        // while loop
+        // int i=1;
+
+        // while(i <= n){
+        //     System.out.println(i);
+        //     i++;
+        // }
+
+        //do while
+
+        int i=1;
+        int n = 5;
+
+        do{
             System.out.println(i);
-        }
+            i++;
+        } while(i <= n);
+        
 
     }
 }
