b3bb1860e35dac592a81be526ff68d71ccbf1b46
Rohit Mahato <rohitmahato88935@gmail.com>
2026-08-05 11:52:03 +0530

    Lecture 5 complete

--- Patterns.java
+++ Patterns.java
@@ -58,7 +58,7 @@ public class Patterns {
 
     // }
 
-    int n = 5;
+    // int n = 5;
 
     // for(int i=1; i<=n; i++){
     //     for(int j=1; j<=i; j++){
@@ -84,7 +84,19 @@ public class Patterns {
     //     System.out.println();
     // }
 
-    
+    int n = 5;
+
+    for(int i=1; i<=n; i++) {
+        for(int j=1; j<=i; j++) {
+            int sum = i+j;
+            if(sum % 2 == 0) {
+                System.out.print("1 ");
+            } else {
+                System.out.print("0 ");
+            }
+        }
+        System.out.println();
+    }
 
     }
 }
