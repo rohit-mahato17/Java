633032ce9e93aef3f8e9d977c301b0e41ed21f63
Rohit Mahato <rohitmahato88935@gmail.com>
2026-08-05 11:07:29 +0530

    Patterns printing

--- Patterns.java
+++ Patterns.java
@@ -60,12 +60,31 @@ public class Patterns {
 
     int n = 5;
 
-    for(int i=1; i<=n; i++){
-        for(int j=1; j<=i; j++){
-            System.out.print(j);
-        }
-        System.out.println();
-    }
+    // for(int i=1; i<=n; i++){
+    //     for(int j=1; j<=i; j++){
+    //         System.out.print(j);
+    //     }
+    //     System.out.println();
+    // }
+
+    // for(int i=n; i>=1; i--) {
+    //     for(int j=1; j<=i; j++) {
+    //         System.out.print(j);
+    //     }
+    //     System.out.println();
+    // }
+
+    // } 
+    //    int k=1;
+    // for(int i=1; i<=n; i++) {
+    //     for(int j=1; j<=i; j++){
+    //         System.out.print(k+" ");
+    //         k++;
+    //     }
+    //     System.out.println();
+    // }
+
+    
 
     }
 }
