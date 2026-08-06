ad5937fe3656ce147f024670082084cd37469501
Rohit Mahato <rohitmahato88935@gmail.com>
2026-08-06 23:47:35 +0530

    java Q Practice

---
+++ Exercise1.java
@@ -0,0 +1,113 @@
+import java.util.*;
+
+public class Exercise1 {
+
+    // //Enter 3 number from the user & make a funtion to print their average.
+    // public static int averageNum(int a, int b, int c){
+    //     int number = (a+b+c) / 3 ;
+    //     return number;
+    // }
+    
+    // public static void main(String[] args){
+    //     Scanner sc = new Scanner(System.in);
+    //     int a = sc.nextInt();
+    //     int b = sc.nextInt();
+    //     int c = sc.nextInt();
+
+    //    int avg = averageNum(a,b,c);
+    //    System.out.println(avg);
+    // }
+
+
+    //Write a function to print the sum of all odde numbers and returns the greater of those two.
+    // public static int sumOfOdd(int num){
+    //     int sumodd = 0;
+    //     for(int i=1; i<=num; i++){
+    //         if(i%2 == 1){
+    //             sumodd += i;
+    //         }
+    //     }
+    //     return sumodd;
+    // }
+    
+    // public static void main(String[] args){
+    //     Scanner sc = new Scanner(System.in);
+    //     int num = sc.nextInt();
+
+    //     int sumodd = sumOfOdd(num);
+    //     System.out.println(sumodd);
+       
+    // }
+
+    //Write a function which takes in 2 number and return as input and returns the greater of those two.
+    // public static void GreaterOfTwoNumber(int a, int b){
+    //     if(a >= b){
+    //         System.out.println("a is the greatest no.");
+    //     } else if(b >= a){
+    //         System.out.println("B is the greatest no.");
+    //     }
+    // }
+    
+    // public static void main(String[] args){
+    //     Scanner sc = new Scanner(System.in);
+    //     System.out.print("Enter the value of a:");
+    //     int a = sc.nextInt();
+    //     System.out.print("Enter the value of b:");
+    //     int b = sc.nextInt(); 
+        
+    //     GreaterOfTwoNumber(a,b);
+    // }
+
+    //Write a function that takes in the radius as input and returns the circumference of a circle.
+    // public static double findCircumference(double radius){
+    //     return 2 * Math.PI * radius;
+    // }
+    
+    // public static void main(String[] args){
+    //     Scanner sc = new Scanner(System.in);
+    //     System.out.print("Enter the value of Radius:");
+    //     double radius = sc.nextDouble();
+        
+    //     double circumference = findCircumference(radius);
+
+    //     System.out.println("Circumference =" +  circumference);
+        
+    // }
+
+    //Write a funtion that takes in age as input and returns if that person is eligible to bote or not. A person of age > 18 is eligible to vote.
+    //  public static int EligibleToVote(int age){
+    //       if(age > 18){
+    //         return 1;
+    //       }else {
+    //         return 0;
+    //       }
+    // }
+    
+    // public static void main(String[] args){
+    //     Scanner sc = new Scanner(System.in);
+    //     System.out.print("Enter you age:");
+    //     int age = sc.nextInt();
+
+    //    int vote = EligibleToVote(age);
+
+    //    if(vote == 1){
+    //     System.out.println("You are aligible to vote");
+    //    }else {
+    //     System.out.println("you are not aligible");
+    //    }
+    // }
+
+    //Write a program to enter the numbers till the user wants and at the end it should display the count of positive, negative and zeros entered.
+    
+    public static int EligibleToVote(int age){
+          
+    }
+    
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter you age:");
+        int age = sc.nextInt();
+      
+    }
+}
+ 
